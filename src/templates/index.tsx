dd0f44a5dd82a1b49ebffd0cf042317d76e821e6
Min Htet Oo <minhtetoo@Mins-MacBook-Air.local>
2024-01-25 18:33:46 +0800

    remove unit tests for v3 v4 and removed templates

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -1,12 +1,10 @@
 import { TemplateRegistry } from "@tradetrust-tt/decentralized-renderer-react-components";
 import { BillOfLadingTemplates } from "./BillOfLading";
-import { CoveringLetterTemplates } from "./CoveringLetter";
 import { InvoiceTemplates } from "./Invoice";
 import { XMLRendererTemplate } from "./XmlRenderer";
 
 export const registry: TemplateRegistry<any> = {
   BILL_OF_LADING: BillOfLadingTemplates,
-  COVERING_LETTER: CoveringLetterTemplates,
   INVOICE: InvoiceTemplates,
   XML_RENDERER: XMLRendererTemplate,
 };

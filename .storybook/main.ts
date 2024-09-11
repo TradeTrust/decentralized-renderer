ad761b62a62cf4f3a34dae800a5d8a01ee55ec0b
PH <68947167+osslgtm@users.noreply.github.com>
2024-09-11 16:35:11 +0800

    chore: upgrade storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,34 @@
-import type { StorybookConfig } from "@storybook/react-webpack5";
+import type { StorybookConfig } from "@storybook/react-webpack5"
+import * as path from "path";
+const toPath = (_path: any) => path.join(process.cwd(), _path);;
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)", "../src/**/*.stories.@(tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
+    "@storybook/addon-webpack5-compiler-babel"
   ],
   framework: {
     name: "@storybook/react-webpack5",
     options: {},
+  },
+  webpackFinal: (config: any) => {
+    return {
+      ...config,
+      // https://github.com/storybookjs/storybook/issues/13277#issuecomment-751747964
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          "@emotion/core": toPath("node_modules/@emotion/react"),
+          "@emotion/styled": toPath("node_modules/@emotion/styled"),
+          "emotion-theming": toPath("node_modules/@emotion/react"),
+        },
+      },
+    };
   },
   docs: {
     autodocs: true,
